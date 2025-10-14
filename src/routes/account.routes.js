d828146fd2da9b192134ca27cfa6bb32dcd0f350
Victor <victorygoralves2019@gmail.com>
2025-10-14 09:56:24 -0300

    ajuste

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -4,12 +4,9 @@ const authenticateToken = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-/** ROTAS PÚBLICAS (não precisam de token) */
 router.post("/register", accountController.register);
 router.post("/login", accountController.login);
 
-
-/** ROTAS PROTEGIDAS (precisam de token) */
 router.use(authenticateToken);
 router.get("/:userId", accountController.getAccountByUserId);
 router.get("/:accountId/balance", accountController.getAccountBalance);
--- src/routes/transfer.routes.js
+++ src/routes/transfer.routes.js
@@ -8,7 +8,6 @@ const {
   validateTransfer
 } = require("../validators/transfer.validator");
 
-// ✅ Todas as rotas que dependem do usuário autenticado precisam do authMiddleware
 router.post("/set_transfer_password", validateSetTransferPassword, authMiddleware, transferController.setTransferPassword);
 router.post("/change_transfer_password", validateChangeTransferPassword, authMiddleware, transferController.changeTransferPassword);
 router.post("/verify_transfer_password", authMiddleware, transferController.verifyTransferPassword);
