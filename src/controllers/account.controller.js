094323ed3a02207cbe813a8fd7eaf3edc8f4700a
Victor <victorygoralves2019@gmail.com>
2025-10-28 09:47:22 -0300

    ajuste, tirado duplicatas de metodos

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -22,25 +22,6 @@ class AccountController {
     }
   }
 
-  async register(req, res, next) {
-    try {
-      const { name, cpf, phone, email, password } = req.body;
-      const user = await accountService.register(name, cpf, phone, email, password);
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  async login(req, res, next) {
-    try {
-      const { cpf, password } = req.body;
-      const token = await accountService.login(cpf, password);
-      res.status(200).json({ token });
-    } catch (error) {
-      next(error);
-    }
-  }
 }
 
 module.exports = new AccountController(); 
